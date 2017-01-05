450247fa35f818a6b7cc898b00b90f9a1ee7a7b3
George Vlahavas <vlahavas@gmail.com>
2017-01-05 21:13:54 +0200

    Create the ClientList and read from DB
    Create the ClientList singleton. When creating the singleton, read the
    client list from the DB. Also provide an update() method that reads the
    client list from the DB and updates the ClientList accordingly.

--- src/ClientList.java
+++ src/ClientList.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
 
 public enum ClientList {
 	CL;
 	
+	private ArrayList<Client> cl;
+	
+	private ClientList() {
+		cl = new ArrayList<Client>();
+		update(); // populate the client list when creating it for the 1st time
+	}
+	
+	// update the client list from the DB
+	public void update() {
+		cl = DBManager.getClientList();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// testing
+		for (Client c: CL.cl) {
+			System.out.println(c);
+		}
 
 	}
 }
