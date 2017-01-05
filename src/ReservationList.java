d2b9c348afbbf3a4fec76f19edb2adbe0bef2397
George Vlahavas <vlahavas@gmail.com>
2017-01-05 22:31:25 +0200

    Populate ReservationList.java

--- src/ReservationList.java
+++ src/ReservationList.java
@@ -1,5 +1,29 @@
-/**
- * Created by McManus on 2017-01-05.
- */
-public class ReservationList {
+import java.util.ArrayList;
+
+public enum ReservationList {
+	RL;
+	
+	private ArrayList<Reservation> rl;
+	
+	private ReservationList() {
+		rl = new ArrayList<Reservation>();
+		update(); // populate the reservation list when creating it for the 1st time
+	}
+	
+	public ArrayList<Reservation> getRL() {
+		return rl;
+	}
+	
+	// update the reservation list from the DB
+	public void update() {
+		rl = DBManager.getReservationList();
+	}
+	
+	public static void main(String[] args) {
+		// testing
+		for (Reservation r: RL.rl) {
+			System.out.println(r);
+		}
+	}
+
 }
