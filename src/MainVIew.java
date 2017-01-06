9a521435c3bce40847202c0d95dc39c5dd716bee
Manos Kakogian <mkakoyan@gmail.com>
2017-01-06 12:56:33 +0200

    basic ui

--- src/MainVIew.java
+++ src/MainVIew.java
@@ -37,7 +37,7 @@ public class MainVIew extends JFrame {
             // JFormDesigner evaluation mark
             dialogPane.setBorder(new javax.swing.border.CompoundBorder(
                 new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+                    "", javax.swing.border.TitledBorder.CENTER,
                     javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
                     java.awt.Color.red), dialogPane.getBorder())); dialogPane.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
@@ -48,7 +48,7 @@ public class MainVIew extends JFrame {
                 contentPanel.setLayout(null);
 
                 //---- label1 ----
-                label1.setText("Hotel Managmen App");
+                label1.setText("Hotel Managment App");
                 contentPanel.add(label1);
                 label1.setBounds(0, 0, 325, 40);
 
