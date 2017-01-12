b7732789927c2e510796143d3d564a65fbab7bc6
George Vlahavas <vlahavas@gmail.com>
2017-01-12 20:51:08 +0200

    Stop printing clients on startup

--- src/HotelManagement.java
+++ src/HotelManagement.java
@@ -3,16 +3,8 @@ import java.util.ArrayList;
 public class HotelManagement {
 
 	public static void main(String[] args) {
-		ArrayList<Client> cl = ClientList.CL.getCL();
-		for (Client c: cl) {
-			System.out.println(c);
-		}
-
 		MainView main = new MainView();
 		main.show();
-
-
-
 	}
 
 }
