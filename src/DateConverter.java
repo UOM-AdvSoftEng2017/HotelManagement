bd01da28c43ecc3928b31d25e511f5d18feb331b
George Vlahavas <vlahavas@gmail.com>
2017-01-14 13:46:43 +0200

    Add some testing code in main

--- src/DateConverter.java
+++ src/DateConverter.java
@@ -1,3 +1,6 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Calendar;
@@ -19,5 +22,18 @@ public class DateConverter {
     // converts Date to LocalDate
     public static LocalDate getLocalDate(Date d) {
         return d.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public static void main(String[] args) throws ParseException {
+        // convert from Date to LocalDate
+        String ds1 = "13/01/2017";
+        DateFormat format = new SimpleDateFormat("d/M/yyyy");
+        Date d1 = format.parse(ds1);
+        LocalDate ld1 = getLocalDate(d1);
+        System.out.println(d1 + " to " + ld1);
+        // convert from LocalDate to Date
+        LocalDate ld2 = LocalDate.of(2017, 02, 15);
+        Date d2 = getDate(ld2);
+        System.out.println(ld2 + " to " + d2);
     }
 }
