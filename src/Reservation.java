ea60323be71ac02bc14580f69f58e1054192451f
George Vlahavas <vlahavas@gmail.com>
2017-01-18 01:11:09 +0200

    Print price and paid attributes
    When printing a Reservation object, also print the price and paid
    attributes.

--- src/Reservation.java
+++ src/Reservation.java
@@ -98,7 +98,7 @@ public class Reservation {
 	
 	@Override
 	public String toString() {
-		return "Reservation [id=" + id + ", start=" + start + ", end=" + end + ", cID=" + cID + ", rID=" + rID + "]";
+		return "Reservation [id=" + id + ", start=" + start + ", end=" + end + ", cID=" + cID + ", rID=" + rID + ", Price=" + price + ", Paid=" + paid + "]";
 	}
 
 	public int getId() {
